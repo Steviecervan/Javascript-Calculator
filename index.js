78a371317236f199866cf2bb32159f417905e7c0
Stevie-Cervan <stevie.cervan06@gmail.com>
2021-12-31 15:44:44 -0700

    Added input fields for numbers

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-let num1 = 8;
-let num2 = 2;
-document.getElementById("num1-el").textContent = num1;
-document.getElementById("num2-el").textContent = num2;
-
+let num1;
+let num2;
 let totalSum;
+
 let sumEl = document.getElementById("sum-el");
+
+function getNumbers() {
+  num1 = document.getElementById("num1-el").value;
+  num1 = parseInt(num1);
+  console.log(num1);
+  num2 = document.getElementById("num2-el").value;
+  num2 = parseInt(num2);
+  console.log(num2);
+}
 
 function add() {
   totalSum = num1 + num2;
-  sumEl.textContent = totalSum;
+  sumEl.textContent = "Total: " + totalSum;
 }
 
 function subtract() {
   totalSum = num1 - num2;
-  sumEl.textContent = totalSum;
+  sumEl.textContent = "Total: " + totalSum;
 }
 
 function divide() {
   totalSum = num1 / num2;
-  sumEl.textContent = totalSum;
+  sumEl.textContent = "Total: " + totalSum;
 }
 
 function multiply() {
   totalSum = num1 * num2;
-  sumEl.textContent = totalSum;
+  sumEl.textContent = "Total: " + totalSum;
 }
